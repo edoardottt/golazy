600b5255feb54c4d44000bfc29f54e2613c2ad21
edoardottt <edoardott@gmail.com>
2022-02-17 11:04:19 +0100

    update

--- golazy.go
+++ golazy.go
@@ -16,10 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see http://www.gnu.org/licenses/.
 
 	@Repository:  https://github.com/edoardottt/golazy
-	
+
 	@Author:      edoardottt, https://www.edoardoottavianelli.it
 */
-
 
 package golazy
 
