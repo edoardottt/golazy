98aad38184e5632de6ed5d21cc6967a57936992d
edoardottt <edoardott@gmail.com>
2022-02-17 10:49:54 +0100

    update

--- internal/version.go
+++ internal/version.go
@@ -1,7 +1,5 @@
 package version
 
-import "github.com/edoardottt/golazy/pkg/golazy"
-
 //Version for golazy
 func Version() string {
 	return "0.1-dev"
---
+++ test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	golazy "github.com/edoardottt/golazy/pkg"
+)
+
+func main() {
+	fmt.Println(golazy.RemoveDuplicateValues([]string{"golazy"}))
+}
